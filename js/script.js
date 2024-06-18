3898d5fbd5305448e594f805a722cb124d64e461
Nelson4111 <aqanakmapa@gmail.com>
2024-06-18 18:16:42 +0800

    hasil Belajar hari ini

--- js/script.js
+++ js/script.js
@@ -1,9 +1,42 @@
-// Toggle class active
+// Toggle class active untuk hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 // Ketika hamburger menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
+
+// toggle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// toggle class active untuk shopping cart
+const shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  shoppingCart.classList.toggle("active");
+  e.preventDefault();
+};
+
+//Klik di luar shopping cart untuk menghilangkan
+const cart = document.querySelector("#shopping-cart-button");
+document.addEventListener("click", function (e) {
+  if (!cart.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
+});
+
+//Klik di luar sedebar untuk menghilangkan search-button
+const search = document.querySelector("#search-button");
+document.addEventListener("click", function (e) {
+  if (!search.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+});
 
 //Klik di luar sedebar untuk menghilangkan nav
 const hamburger = document.querySelector("#hamburger-menu");
@@ -12,3 +45,27 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+// Modal Box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// Klik Tombal Close Modal
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+// Klik di luar modal untuk menghilangkan modal
+window.onclick = (e) => {
+  if (e.target == itemDetailModal) {
+    modal.style.display = "none";
+  }
+};
